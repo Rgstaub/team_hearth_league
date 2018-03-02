3f622df4d9d7eca1dc0ee140d32f5e84883a1ae5
Rgstaub <rgstaub@gmail.com>
2018-03-01 22:16:28 -0500

    deploy

--- api/public_routes.js
+++ api/public_routes.js
@@ -3,7 +3,7 @@ const UserController = require('../controllers/user_controller.js');
 
 module.exports = function(app) {
 
-  app.post('public/newUser/', (req, res) => {
+  app.post('/public/newUser/', (req, res) => {
     UserController.createUser(req.body)
     .then( newUser => res.json(newUser ))
     .catch( err => res.json(err))
