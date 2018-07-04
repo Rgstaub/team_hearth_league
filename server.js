1334cb1f4131e8dddf32cdd431ea179725c42348
Rgstaub <rgstaub@gmail.com>
2018-07-04 13:57:49 -0400

    heroku

--- server.js
+++ server.js
@@ -39,6 +39,12 @@ app.use(cors());
 // Static directory
 app.use(express.static(path.join(__dirname, '../../thl-app/build')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.use(session({
   secret: '-v^-itsasecrettoeveryone-^v-',
